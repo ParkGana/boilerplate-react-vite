4128a14d05b9afd5793e65d3e6eb3dd02a885fbe
ParkGana <gnchoco97@gmail.com>
2026-03-27 12:06:02 +0900

    Axios 설정

---
+++ src/api/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+/* Request Interceptor */
+api.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error),
+);
+
+/* Response Interceptor */
+api.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error),
+);
+
+export default api;
