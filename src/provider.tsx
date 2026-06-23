a9d37280d3095a2f187931bd63d9fb53052c0a2b
ParkGana <gnchoco97@gmail.com>
2026-06-23 13:47:18 +0900

    TanStack Query 설정 로직 보완

--- src/provider.tsx
+++ src/provider.tsx
@@ -3,9 +3,9 @@ import QueryProvider from './providers/queryProvider';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <AuthProvider>
-      <QueryProvider>{children}</QueryProvider>
-    </AuthProvider>
+    <QueryProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryProvider>
   );
 };
 
--- src/providers/queryProvider.tsx
+++ src/providers/queryProvider.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
 
